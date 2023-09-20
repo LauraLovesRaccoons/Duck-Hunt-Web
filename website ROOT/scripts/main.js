54f28230fb1d023a275a3583e99abe4d4273c252
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-09-20 14:53:59 +0200

    ducks are moving
    happy

--- website ROOT/scripts/main.js
+++ website ROOT/scripts/main.js
@@ -4,17 +4,14 @@
 let posTop = 0;
 let posLeft = 0;
 
-let duck1 = document.getElementById("duck1");
+// let duck1 = document.getElementById("duck1");
+
 duck1.style.top = posTop;   // start always at 0
 duck1.style.top = posLeft;  // start always at 0
 duck2.style.top = posTop;   // start always at 0
 duck2.style.top = posLeft;  // start always at 0
 duck3.style.top = posTop;   // start always at 0
 duck3.style.top = posLeft;  // start always at 0
-
-randomPosition("duck1");
-randomPosition("duck2");
-randomPosition("duck3");
 
 
 
@@ -25,3 +22,12 @@ function randomPosition(callingAnID){
     insideFunction.style.top = Math.random()*500 + "px";
     insideFunction.style.left = Math.random()*700 + "px";
 }
+
+// this is needed because callingAnID will be empty if I call it w/o arguments
+setInterval(moveDucks, 500)
+ function moveDucks (){
+    randomPosition("duck1");
+    randomPosition("duck2");
+    randomPosition("duck3");
+ }
+
