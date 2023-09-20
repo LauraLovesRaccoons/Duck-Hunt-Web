bb16d39fecba674782664b7ae83fb7c86e8cebe7
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-09-20 14:19:09 +0200

    duck starts now at 0;0 in code
    ditto

--- website ROOT/scripts/main.js
+++ website ROOT/scripts/main.js
@@ -5,6 +5,8 @@ let posTop = 0;
 let posLeft = 0;
 
 let duck1 = document.getElementById("duck1");
+duck1.style.top = posTop;   // start always at 0
+duck1.style.top = posLeft;  // start always at 0
 duck1.style.top = Math.random()*500 + "px";
 duck1.style.left = Math.random()*700 + "px";
 
